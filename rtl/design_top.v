9e962546fa70f4435b2ee06eb6295d12d5458d20
verylaggg <verylag0401@gmail.com>
2025-08-01 12:22:24 +0800

    add rtl/ model/

---
+++ rtl/design_top.v
@@ -0,0 +1,22 @@
+/******************************************************************************
+* |----------------------------------------------------------------------------|
+* |                      Copyright (C) 2024-2025 VeryLag.                      |
+* |                                                                            |
+* | THIS SOURCE CODE IS FOR PERSONAL DEVELOPEMENT; OPEN FOR ALL USES BY ANYONE.|
+* |                                                                            |
+* |   Feel free to modify and use this code, but attribution is appreciated.   |
+* |                                                                            |
+* |----------------------------------------------------------------------------|
+*
+* Author : VeryLag (verylag0401@gmail.com)
+* 
+* Creat : 2025/04/02
+* 
+* Description : empty top model
+* 
+******************************************************************************/
+
+`timescale 1ns/1ns
+module design_top ();
+
+endmodule
